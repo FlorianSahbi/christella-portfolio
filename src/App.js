1de0414c54cd5d26f9c66d63f384f9a4069c552d
FlorianSahbi <florian.sahbi@gmail.com>
2020-02-23 15:01:54 +0100

    ADD : Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Reset.css";
 import "./App.css";
 
 import apesAsso from "./images/apes-asso.png";
@@ -18,26 +19,34 @@ const Loader = () => {
 
 const Footer = () => {
   return (
-    <div className="header">
-      <div className="name"> Christella Levieux </div>{" "}
-      <div className="name"> christella.levieux @icloud.com </div>{" "}
-      <a
-        href="https://twitter.com/christella_kl/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="name"
-      >
-        twitter{" "}
-      </a>{" "}
-      <a
-        href="https://www.linkedin.com/in/christella-levieux/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="name"
-      >
-        Linkedin{" "}
-      </a>{" "}
-    </div>
+    <footer>
+      <div className="leftnav">
+        <div className="footer-name">
+          Christella Levieux <span className="dot-footer">.</span>
+        </div>
+        <div className="name"> christella.levieux @icloud.com </div>{" "}
+      </div>
+      <div className="rightnav">
+        <a
+
+          href="https://twitter.com/christella_kl/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="name"
+        >
+          Twitter{" "}
+        </a>{" "}
+        <a
+        style={{marginLeft: "2.5rem", color: "#40404C"}}
+          href="https://www.linkedin.com/in/christella-levieux/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="name"
+        >
+          Linkedin{" "}
+        </a>{" "}
+      </div>
+    </footer>
   );
 };
 
@@ -55,12 +64,16 @@ const Menu = () => {
   );
 };
 
+const MoreButton = props => {
+  return <p className="more">Voir le projet</p>;
+};
+
 const Card = ({ name, description, img }) => {
   return (
     <div className="works">
       <div className="title">{name}</div>
       <div className="description">
-        {description} <p>Voir le projet</p>
+        {description} <MoreButton />
       </div>
       <div className="image">
         <img src={img} alt="ok" />
